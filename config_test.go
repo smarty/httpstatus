939aa439c011ffdb3f92c90d3de906646c78111d
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2020-05-13 07:34:22 -0600

    Additional test coverage around SQL.

---
+++ config_test.go
@@ -0,0 +1,41 @@
+package httpstatus
+
+import (
+	"database/sql"
+	"testing"
+	"time"
+
+	"github.com/smartystreets/assertions/should"
+	"github.com/smartystreets/gunit"
+)
+
+func TestConfigFixture(t *testing.T) {
+	gunit.Run(new(ConfigFixture), t)
+}
+
+type ConfigFixture struct {
+	*gunit.Fixture
+}
+
+func (this *ConfigFixture) Setup() {
+}
+
+func (this *ConfigFixture) TestWhenSQLDBProvided_UseBuiltInHealthCheck() {
+	sqlHandle := &sql.DB{}
+
+	handler := New(Options.SQLHealthCheck(sqlHandle))
+
+	// this panics because the handle isn't connected to a real DB, that's how we know it's working
+	this.So(handler.Listen, should.Panic)
+}
+
+func (this *ConfigFixture) TestWhenNoDefaultHealthCheckProvided_ItShouldReturnHealthy() {
+	handler := New()
+
+	go func() {
+		time.Sleep(time.Millisecond)
+		_ = handler.Close()
+	}()
+
+	this.So(handler.Listen, should.NotPanic)
+}
